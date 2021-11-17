3de5a938c3acbdce9d4447b443399bfbf551318f
FranciscoSampa <92555284+FranciscoSampa@users.noreply.github.com>
2021-11-17 00:58:09 -0300

    Add files via upload

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            <strong><span className='text-danger'></span> </strong>
+            <i></i>
+            <strong> </strong>
+        </span>
+    </footer>
